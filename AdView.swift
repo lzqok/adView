0db85b85aca1cdeea4a8ffbb57d825b471579df4
EmptyBrain <emptybrain@192.168.0.110>
2016-12-20 23:09:19 +0800

    add Notes

--- AdView.swift
+++ AdView.swift
@@ -1,24 +1,28 @@
 //
 //  AdView.swift
-//  DrinkChart
-//
-//  Created by 技术部 on 2016/12/20.
-//  Copyright © 2016年 技术部. All rights reserved.
+//  
+//  Created by lzqok on 2016/12/20.
+//  Copyright © lzqok lzqok. All rights reserved.
 //
 
 import UIKit
 
+/**
+ * 声明代理协议 OC访问swift代理方法需要在声明协议前添加@objc
+ */
 @objc
 protocol AdViewDelegate {
     func adIndexSelect(index:NSInteger)
 }
 
 class AdView: UIView ,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
-    
+    //声明collectionView
     var collectionView : UICollectionView?
+    //声明数组用于接收控制器传过来的数组进行
     var adArray : NSMutableArray?{
         willSet{
             self.adArray = newValue
+            //将数组第一个元素添加到数组最后用于解决从最后一个元素滚动到第一个元素动画不协调问题
             if (self.adArray?.count)! > 1 {
                 self.adArray?.add(self.adArray?.object(at: 0) as Any)
             }
@@ -27,15 +31,12 @@ class AdView: UIView ,UICollectionViewDelegate,UICollectionViewDataSource,UIColl
     }
     var delegate : AdViewDelegate?
     
-    var timer : Timer = Timer()
+    var timer : Timer = Timer() //初始化定时器
     
     typealias AdViewBlock = (_ index:NSInteger)->Void
     
-    override init(frame:CGRect){
-        super.init(frame: frame)
-        setViews()
-    }
     
+    //view布局的一些设置
     func setViews(){
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize.init(width: self.bounds.width, height: self.bounds.height)
@@ -55,7 +56,13 @@ class AdView: UIView ,UICollectionViewDelegate,UICollectionViewDataSource,UIColl
         }
         self.addTimer()
     }
+    //代码中通过init创建view时执行
+    override init(frame:CGRect){
+        super.init(frame: frame)
+        setViews()
+    }
     
+    //nib中custom class 引用该类时执行
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setViews()
@@ -65,7 +72,7 @@ class AdView: UIView ,UICollectionViewDelegate,UICollectionViewDataSource,UIColl
         super.awakeFromNib()
 
     }
-    
+    //创建一个定时器timer1并将timer1赋给timer
     func addTimer(){
         let timer1 = Timer.init(timeInterval: 3, target: self, selector: #selector(nextPage), userInfo: nil, repeats: true)
         RunLoop.current.add(timer1, forMode: .commonModes)
@@ -82,7 +89,7 @@ class AdView: UIView ,UICollectionViewDelegate,UICollectionViewDataSource,UIColl
         collectionView?.scrollToItem(at: currentIndexPath!, at: .top, animated: true)
         return currentIndexPath! as NSIndexPath
     }
-    
+    //自动滚动到下一页
     func nextPage(){
         
         let indexPath = returnIndexPath()
@@ -90,14 +97,14 @@ class AdView: UIView ,UICollectionViewDelegate,UICollectionViewDataSource,UIColl
         if row == adArray?.count {
             row = 0
         }
-        
+        //flag 标示如果是最后一个者滚动到第一个位置不添加动画
         var flag = true
         if(row == 0){
             flag = false
-            
+            //滚动到第一个
             let nextIndexPath = NSIndexPath.init(row: row, section:0)
             collectionView?.scrollToItem(at: nextIndexPath as IndexPath, at: .top, animated: flag)
-            
+            //接着动画滚到到第二个位置
             let nextIndexPath1 = NSIndexPath.init(row: row+1, section:0)
             collectionView?.scrollToItem(at: nextIndexPath1 as IndexPath, at: .top, animated: true)
             
@@ -139,7 +146,7 @@ class AdView: UIView ,UICollectionViewDelegate,UICollectionViewDataSource,UIColl
     }
     
 }
-
+//UICollectionViewCell 可自定义此处实现图文并茂的广告
 class AdviewCollectionViewCell : UICollectionViewCell {
     var titleLabel = UILabel()
     override init(frame: CGRect){
